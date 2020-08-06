6d8ae80cbd9c0e68aad68f20ed5aea6177bf611d
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-08-06 09:25:27 +0400

    Initial commit

---
+++ src/main/java/com/company/helloworld/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.company.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="main.datasource")
+	DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+}
